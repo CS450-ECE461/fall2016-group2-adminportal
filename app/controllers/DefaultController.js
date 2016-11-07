6037fef9c9bc22d6bc6eddc81c56c0a22a38c78f
eburgun <eburgun@iupui.edu>
2016-11-07 16:54:02 -0500

    Remove merge conflict comment

--- app/controllers/DefaultController.js
+++ app/controllers/DefaultController.js
@@ -9,16 +9,8 @@ function DefaultController () {
 blueprint.controller (DefaultController);
 
 DefaultController.prototype.landing = function () {
-<<<<<<< HEAD
-    
-    
     return function (req, res) {
         return res.redirect ('/login');
-=======
-    
-    return function (req, res) {
-        return res.render ('login.pug', {message: "Welcome to Hive Login"});
->>>>>>> issue15
     };
 };
 
