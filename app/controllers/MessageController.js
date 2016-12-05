a477bfa608fd9d8c1ba4699ca14507f23508f319
eburgun <eburgun@iupui.edu>
2016-12-05 02:33:34 -0500

    FirstMessageController

--- app/controllers/MessageController.js
+++ app/controllers/MessageController.js
@@ -11,3 +11,27 @@ function MessageController () {
 
 blueprint.controller(MessageController);
 
+MessageController.prototype.fetch = function () {
+    return function (req, res) {
+        var token = req.user;
+
+        request
+            .get('localhost:5000/v1/messages')
+            .set('Authorization', 'bearer ' + token)
+            .end (function(err,resp){
+            if(err) {
+                console.log(err);
+                return res.render('dashboard.pug',{message: "Unable to fetch messages."});
+            } else {
+                var list = {};
+                for (var x in resp.body.messages) {
+                    list[x] = resp.body.messages[x];
+                }
+                console.log(list);
+                return res.render('messages.pug',{list: list});
+            }
+        });
+    };
+};
+
+module.exports = exports = MessageController;
