9711986ca46244345e861f54c5bf2068818638a7
Danny Peck <danieljpeck93@gmail.com>
2016-12-12 02:22:16 -0500

    removed bad comments

--- public_html/scripts/userList.js
+++ public_html/scripts/userList.js
@@ -1,8 +1,6 @@
 $.getJSON( "/users", function( data ) {
     $("#user-list").append("<tr> <th> UserName </th> <th> Email </th> <th> Job Title </th> </tr>");
     $.each( data, function( index, item ) {
-        // if your IDE complains about this, ignore it, it is wrong
-        //$( "#user-list").append("<li id='" + key + "'>" + val + "</li>" );
         $("#user-list").append("<tr> id='" + index + "'><td>" + item.username + "</td><td>" + item.email + "</td><td>" + item.job_title +"</td></tr>");
     });
 });
